5979a06251e6d00343f9ee43d39ea87e7286477a
jahidprog <jahid.prog@gmail.com>
2026-01-04 13:09:10 +0600

    first commit

---
+++ Session 01 /hello/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace abc
+{
+    struct Point
+{
+    public int X;
+}
+
+Point p1 = new Point();
+p1.X = 5;
+
+Point p2 = p1;
+p2.X = 10;
+
+Console.WriteLine(p1.X);
+Console.WriteLine(p2.X);
+}
