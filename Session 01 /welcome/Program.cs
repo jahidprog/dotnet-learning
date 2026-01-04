cb7b865755368b803a93020efafd5f647ab433be
jahidprog <jahid.prog@gmail.com>
2026-01-04 13:24:37 +0600

    01-welcome

---
+++ Session 01 /welcome/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Welcome
+{
+    class Student
+    {
+        public string Name;
+        public int Age;
+        public int Roll;
+
+        public Student(string name, int age, int roll){
+            Name = name;
+            Age = age;
+            Roll = roll;
+        }
+
+        public void StudentDetails()
+        {   
+            Console.WriteLine(Name);
+            Console.WriteLine(Age);
+            Console.WriteLine(Roll);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student("Jahid", 26, 11);
+            s1.StudentDetails();
+        }
+    }
+
+    
+}
